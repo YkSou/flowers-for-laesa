2d34c786ea761b3e89f45898416258ee0c10a4c3
Miserable. <Tegardesktop83@gmail.com>
2026-09-07 13:36:40 +0700

    Flowers for laesa

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `I Have Something`.split('')
+const text = `Bebeee... I Hv Smthn 4U`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
